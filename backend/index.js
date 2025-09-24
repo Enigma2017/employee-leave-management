930859cdc1d6471daf002b9b4f4ca0d5057a872d
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2025-09-24 14:42:30 +0300

    added dotenv, nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
+import express from 'express'
+import 'dotenv/config'
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
