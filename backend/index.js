d784a1a1f63f3661df9783e789adb989be68e5db
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2025-09-24 14:55:17 +0300

    added parse body, corses

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,23 @@ import express from 'express'
 import 'dotenv/config'
 const app = express()
 
+// corses
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
+
+app.use(express.json()) // Middleware to parse JSON bodies
+app.post('/data', (req, res) => {
+  const receivedData = req.body
+  console.log(receivedData)
+  res.sendStatus(200)
+}) // Endpoint to receive data
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
