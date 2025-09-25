6d36b0bf223fab2634f7b2f2cbfd32b22e357be9
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2025-09-25 15:08:16 +0300

    added functions for interaction with the users

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,19 +1,23 @@
 import routes from 'express' // import express
-import { getAllUsers, createUsersTable, addUser } from '../services/users.services.js'
+import { getAllUsers, createUsersTable, addUser, editUser, deleteUser } from '../services/users.services.js'
 const router = routes.Router() // create a router instance
+//(async () => await createUsersTable())() // ensure the users table exists
+async function ensureUsersTable() {
+  try {
+    await createUsersTable()
+  } catch (error) {
+    console.error("Error creating users table:", error)
+  }
+}
+
+ensureUsersTable();
 
 router.get('/', (req, res) => {
   res.send('Hello World from routes')
 })
 
-router.post('/data', (req, res) => {
-  const receivedData = req.body
-  console.log(receivedData)
-  res.sendStatus(200)
-}) // Endpoint to receive data
-
 router.get('/testdb', async (req, res) => {
-  await createUsersTable() // ensure the users table exists 
+  
   // create random user data
   const randomName = `User${Math.floor(Math.random() * 1000)}`
   const randomEmail = `user${Math.floor(Math.random() * 1000)}@example.com`
@@ -27,8 +31,58 @@ router.get('/testdb', async (req, res) => {
   res.json(users)
 
   // create the users table if it doesn't exist
-  await createUsersTable()
+  //await createUsersTable()
 
 })
+
+// users RESTful API endpoints
+router.get('/users', async (req, res) => {
+  try {
+    const users = await getAllUsers()
+    res.json(users)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+// create a new user
+router.post('/user', async (req, res) => {
+  const { name, email, role, password } = req.body
+  try {
+    const newUser = await addUser(name, email, role, password)
+    res.status(201).json(newUser)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error') 
+  }
+})
+
+// edite a user
+router.put('/user/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, email, role, password } = req.body
+  try {
+    const updatedUser = await editUser(id, name, email, role, password)
+    res.json(updatedUser)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')   
+  }  
+  
+})
+
+// delete a user
+router.delete('/user/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    await deleteUser(id)
+    res.status(204).send() // No Content
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')   
+  }
+})
+
 
 export default router
--- backend/services/users.services.js
+++ backend/services/users.services.js
@@ -26,4 +26,19 @@ export async function addUser(name, email, role, password) {
     const values = [name, email, role, password];
     const result = await pool.query(query, values);
     return result.rows[0];
-}   
+}
+
+// function to edite a user
+export async function editUser(id, name, email, role, password) {
+    const query = `UPDATE users SET name=$1, email=$2, role=$3, password=$4 WHERE id=$5 RETURNING *`;
+    const values = [name, email, role, password, id];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+}
+
+// function to delete a user
+export async function deleteUser(id) {
+    const query = `DELETE FROM users WHERE id=$1`;
+    const values = [id];
+    await pool.query(query, values);
+}
